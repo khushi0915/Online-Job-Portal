e5765f117a93a230590ebe50244e4edcf20e7d0b
khushi0915 <khushi6428@gmail.com>
2024-03-28 14:33:38 +0530

    ojp

---
+++ System/constants/settings.php
@@ -0,0 +1,14 @@
+<?php
+$actual_link = $_SERVER['HTTP_HOST'];
+
+$default_timezone = '';
+$smtp_host = '';
+$smtp_user = '';
+$smtp_pass = '';
+
+$contact_mail = 'priyaswati414@gmail.com';
+
+$fb  = '#';
+$tw = '#';
+$ig = '#';
+?>
